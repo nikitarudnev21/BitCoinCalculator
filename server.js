05dec26484ac41b712c2bd34a7650528231d150f
Opilane TTHK <opilane@tthk.ee>
2020-09-17 15:21:51 +0300

    v2

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const bodyPareser = require('body-parser')
-app.use(bodyPareser.urlencoded({extended: true}))
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
 const PORT = process.env.PORT || 3002
 
 app.get('/', (req, res) => {
@@ -10,9 +10,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
+    const num1 = parseInt(req.body.num1)
+    const num2 = parseInt(req.body.num2)
     console.log('Number 1', req.body.num1)
-    console.log('Number 2',  req.body.num2)
-    res.send('<b>'+(req.body.num1 +req.body.num2)+'</b>');
+    console.log('Number 2',  req.body.num1)
+    const result = num1 + num2
+    res.json({result})
 })
 
 app.listen(PORT, () => {
